58ca9cd2a960094559b72c8b12e18ce92651e2bd
carloscorva <carloscorva@gmail.com>
2018-04-02 00:01:09 +0200

    URL Authorization

--- src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -2,6 +2,7 @@ package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -20,4 +21,13 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
                 roles("USER");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/delete/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
 }
